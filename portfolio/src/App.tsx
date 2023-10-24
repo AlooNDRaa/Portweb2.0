93e2eeecdf7dd8253b72fab89d6d9ef37287a1e8
Alondra <alonngadea@gmail.com>
2023-10-24 20:06:05 -0300

    Refactor:desing tastes

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { HOME } from './assets/componentes/home'
+
+function App() {
+  
+  return (
+    <>
+      <HOME/>
+       
+    </>
+  )
+}
+
+export default App
---
+++ portfolio/src/assets/componentes/home.tsx
@@ -0,0 +1,25 @@
+export function HOME() {
+    return  (
+        <>
+        {/* <div classNameName="cont-home">
+        <h1 classNameName="titular">Gadea Alondra Juliana</h1>
+        <h3>FullStack Developer</h3>
+        </div> */}
+        <div className="firefly"></div>
+        <div className="firefly"></div>
+        <div className="firefly"></div>
+        <div className="firefly"></div>
+        <div className="firefly"></div>
+        <div className="firefly"></div>
+        <div className="firefly"></div>
+        <div className="firefly"></div>
+        <div className="firefly"></div>
+        <div className="firefly"></div>
+        <div className="firefly"></div>
+        <div className="firefly"></div>
+        <div className="firefly"></div>
+        <div className="firefly"></div>
+        <div className="firefly"></div>
+        </>
+    )
+}
