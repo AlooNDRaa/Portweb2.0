878588a48c324a367b894599c9b2f3ea41155ae8
Alondra <alonngadea@gmail.com>
2023-10-24 21:56:51 -0300

    Refactor: animations with css and React components

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,14 +1,26 @@
 import './App.css'
-import { HOME } from './assets/componentes/home'
+import './index.css'
+import { useState, useEffect } from 'react';
+import { Animation } from './assets/componentes/animation'
+import { Me } from './me'
 
 function App() {
-  
+  const [showAnimation, setShowAnimation] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowAnimation(false); 
+    }, 3000);
+
+    return () => clearTimeout(timeout); 
+  }, []);
+
   return (
     <>
-      <HOME/>
-       
+      {showAnimation && <Animation />}
+      {!showAnimation && <Me />}
     </>
   )
 }
 
-export default App
+export default App;
---
+++ portfolio/src/me.tsx
@@ -0,0 +1,15 @@
+import '../src/index.css'
+
+
+export function Me() {
+   return(
+    <>
+    <div className="cont-me">
+    <div className="cont-name">
+        <h1>Gadea Alondra Juliana</h1>
+             <p>FullStack Developer</p>
+    </div>
+     </div>
+    </>
+   ) 
+}
